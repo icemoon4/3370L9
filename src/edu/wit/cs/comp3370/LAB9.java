d75194051175e945c02025bdeffe460072b0720f
Rachel Palmer <palmerr@wit.edu>
2017-07-25 21:49:15 -0400

    Created MST with Prim's algorithm

--- src/edu/wit/cs/comp3370/LAB9.java
+++ src/edu/wit/cs/comp3370/LAB9.java
@@ -2,6 +2,11 @@ package edu.wit.cs.comp3370;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /* Calculates the minimal spanning tree of a graph 
@@ -9,16 +14,112 @@ import java.util.Scanner;
  * Wentworth Institute of Technology
  * COMP 3370
  * Lab Assignment 9
- * 
+ * Rachel Palmer
  */
 
 public class LAB9 {
 	
-	// TODO document this method
+	/**
+	 * FindMst first uses graph g and adds possible neighbors to every vertex (possible
+	 * neighbors being those that are within distance less than epsilon). Then, it calls
+	 * prim(Graph g) to add the MST edges to graph. These are the edges that return the
+	 * smallest weight that will traverse the entire graph. The method prim uses Prim's
+	 * algorithm and constructs a min-heap based on the costs of the vertices from the
+	 * starting vertex (in this case, the value at index 0 is the start vertex). The
+	 * vertex of smallest cost is removed from the heap each time, and the cost of its
+	 * neighbors' is calculated. After every neighbor cost is updated, pushdown and
+	 * pullup is called to re-organize the heap. Finally, every parent-child pair is
+	 * added as an edge to the graph.
+	 * @param g		graph generated from the points files
+	 */
 	public static void FindMST(Graph g) {
-		// TODO generate edges from graph vertices
-		
-		// TODO populate graph with MST edges
+		//generate possible edges, as neighbors to vertices
+		Vertex[] v = new Vertex[g.getVertices().length];
+		v = g.getVertices();
+		for(int i = 0; i < v.length; i++){
+			for(int j = i+1; j < v.length; j++){
+				if(distance(v[i], v[j]) < g.epsilon){
+					v[i].neighbors.add(v[j]);
+					v[j].neighbors.add(v[i]);
+				}
+			}
+		}
+		//populate graph with MST edges
+		prim(g);
+	}
+	
+	public static void prim(Graph g){
+		ArrayList<Vertex> vertex = new ArrayList<Vertex>();
+		for(int i = 0; i < g.getVertices().length; i++){
+			vertex.add(g.getVertices()[i]);
+		}
+		vertex.get(0).c = 0;
+
+		while(vertex.size() > 0){
+			Vertex u = vertex.remove(0); //remove 0
+			//call pushdown on index 0 unless arraylist is now empty
+			if(!vertex.isEmpty()){
+				pushdown(vertex, 0);
+			}
+			for(int i = 0; i<u.neighbors.size(); i++){
+				if(vertex.contains(u.neighbors.get(i)) && distance(u, u.neighbors.get(i)) < u.neighbors.get(i).c){
+					u.neighbors.get(i).c = distance(u, u.neighbors.get(i));
+					u.neighbors.get(i).parent = u;
+					pushdown(vertex,vertex.indexOf(u.neighbors.get(i)));
+					pullup(vertex,vertex.indexOf(u.neighbors.get(i)));
+				}
+			}
+		}
+		//store vertices in an array
+		Vertex[] v = new Vertex[g.getVertices().length];
+		v = g.getVertices();
+		//all parent-child pairs are added as edges
+		for(int w = v.length-1; w >= 0; w--){
+			if(v[w].parent != null){
+				g.addEdge(v[w], v[w].parent);
+			}
+		}
+	}
+	
+	public static double distance(Vertex a, Vertex b){
+		return Math.sqrt(Math.pow(a.x-b.x, 2) + Math.pow(a.y-b.y, 2));
+	}
+	
+	public static ArrayList<Vertex> pullup(ArrayList<Vertex> a, int i){
+		int parenti = (i-1)/2;
+		if(a.get(i).c < a.get(parenti).c){ 
+			Vertex temp = a.get(parenti);
+			a.set(parenti, a.get(i));
+			a.set(i, temp); 
+			if(parenti != 0)
+				pullup(a, parenti);
+		}
+		return a;
+	}
+	
+	public static ArrayList<Vertex> pushdown(ArrayList<Vertex> a, int i){
+		int lchild = 2*i + 1;
+		int rchild = 2*i + 2;
+		int mini = i;
+		if(lchild < a.size()){
+			if(rchild < a.size()){ //if the rchild is valid
+				if(a.get(lchild).c <= a.get(rchild).c && a.get(lchild).c < a.get(mini).c)
+					mini = lchild;
+				else if(a.get(rchild).c <= a.get(lchild).c && a.get(rchild).c < a.get(mini).c)
+					mini = rchild;
+			}
+			else{ //if rchild is not valid
+				if(a.get(lchild).c < a.get(mini).c)
+					mini = lchild;
+			}
+			if(mini != i){
+				Vertex temp = a.get(mini);
+				a.set(mini, a.get(i));
+				a.set(i, temp);
+				pushdown(a, mini); 
+			}
+		}
+		return a;
 	}
 	
 	/********************************************
--- src/edu/wit/cs/comp3370/Vertex.java
+++ src/edu/wit/cs/comp3370/Vertex.java
@@ -1,7 +1,18 @@
 package edu.wit.cs.comp3370;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 // represents a vertex in a graph, including a unique ID to keep track of vertex
-public class Vertex {
+public class Vertex{
+	double c;
+	Vertex parent;
+	ArrayList<Vertex> neighbors;
+	public Vertex(){
+		c = Double.POSITIVE_INFINITY;
+		parent = null;
+		neighbors = new ArrayList<Vertex>();
+	}
 	
 	/********************************************
 	 * 
@@ -12,4 +23,5 @@ public class Vertex {
 	public double x;
 	public double y;
 	public int ID;
+	
 }
